1e3b55cefbf9264a6fa14d5163f08d3fda492b79
jackira01 <s4bu3z0@gmail.com>
2023-09-29 15:16:55 -0500

    deleted extra files

--- app/modal/page.tsx
+++
@@ -1,6 +0,0 @@
-import LocalDate from '@/components/LocalDate/LocalDate';
-import TypewriterEffect from '@/components/TypeWriterEffect/TypeWriterEffect';
-
-export default function Example() {
-  return <TypewriterEffect />;
-}
