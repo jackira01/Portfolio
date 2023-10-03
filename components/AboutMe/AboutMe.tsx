b3620e2e843534367c9c14c919f130b834614e40
jackira01 <s4bu3z0@gmail.com>
2023-10-03 12:56:05 -0500

    ➕add typeWritter, update AboutMe.

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -1,14 +1,24 @@
+'use client';
+
 import { inconsolata } from '@/app/font';
+import Typewriter from 'typewriter-effect';
 
 export default function AboutMe() {
   return (
-    <div className={`text-left select-none text-white px-7  ${inconsolata.className}`}>
+    <div
+      className={`text-left select-none text-white px-7  ${inconsolata.className}`}
+    >
       <h2 className='font-bold'>Acerca De mi.</h2>
-      <p>
-        Soy Jhoan Alvarez un Full Stack Web Developer, me encanta trabajar en equipo y me
-        siento cómodo tanto en entornos ágiles como en proyectos más
-        estructurados. 
-      </p>
+      <Typewriter
+        onInit={(typewriter) => {
+          typewriter
+            .changeDelay(50)
+            .typeString(
+              'Full Stack Developer con experiencia en Desarrollo web Freelancer en Front-End y Back-End. Experto en metodologías ágiles, GIT, bases de datos, algoritmos y Frameworks CSS. Destrezas en JavaScript, React, MongoDB, SQL, Bootstrap, etc. Colaborador en proyectos grupales, desarrollando habilidades blandas como Adaptabilidad, Trabajo en equipo y comunicación.'
+            )
+            .start();
+        }}
+      />
     </div>
   );
 }
