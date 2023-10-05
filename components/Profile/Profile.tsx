4b3ea84b1d6dea27b91da222241eb5996bd64a52
jackira01 <s4bu3z0@gmail.com>
2023-10-05 15:42:37 -0500

    ✅update Profile Component.

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,10 +1,15 @@
+'use client';
+
 import Image from 'next/image';
 import { profileIcon } from '../imagesImports/ImageImport';
 import { inconsolata } from '@/app/font';
+import Typewriter from 'typewriter-effect';
 
 export default function Profile() {
   return (
-    <div className='duration-500 skew-x-6 group hover:-skew-x-0 hover:translate-x-2'>
+    <div
+      className={`duration-500 skew-x-6 group hover:-skew-x-0 hover:translate-x-2 select-none ${inconsolata.className}`}
+    >
       <div className="group-hover:duration-400 relative rounded-2xl w-72 h-36 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-72 before:h-32 before:-z-10">
         <Image
           className='border-4 border-[#6b6a6a] rounded-full'
@@ -12,11 +17,21 @@ export default function Profile() {
           src={profileIcon}
           alt='left-icon'
         />
-        <p
-          className={`select-none text-amber-300 block animate-typing whitespace-nowrap overflow-hidden w-[33ch] text-center ${inconsolata.className}`}
-        >
-          - Desarrollador Web JavaScript -
-        </p>
+
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .changeDelay(100)
+              .typeString('Desarrollador Web Front-End.')
+              .pauseFor(1500)
+              .deleteChars(10)
+              .typeString('Back-end.')
+              .pauseFor(1500)
+              .deleteChars(9)
+              .typeString('JavaScript.')
+              .start();
+          }}
+        />
       </div>
     </div>
   );
