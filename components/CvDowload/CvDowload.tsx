1dc9cabc3e0134a768d93f8456beb8b215825296
Jhoan Beru <96591137+jackira01@users.noreply.github.com>
2024-06-04 07:44:39 -0500

    Update CvDowload.tsx

--- components/CvDowload/CvDowload.tsx
+++ components/CvDowload/CvDowload.tsx
@@ -3,7 +3,7 @@ export default function CvDowload() {
     <div className='flex flex-col items-center justify-center align-middle select-none'>
       <div>
         <a
-          href="https://drive.google.com/file/d/1iZHKGmylBC_iEzlkLJr3J57FrVXFLvLU/view"
+          href="https://drive.google.com/file/d/1JOTcSFttUo4HTOK9SxfhiSAJWWvXilTb/view?usp=drivesdk"
           download
           target="blank"
           className=' group-hover:bg-opacity-50 items-center cursor-pointer group relative flex flex-col gap-1.5 bg-opacity-80 dark:text-white text-slate-900 rounded-3xl hover:bg-opacity-70 transition font-semibold '
